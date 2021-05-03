cd6d1f28e2f7a4ee2bba9d02e764f987f532e9ae
Bryant <33697753+BryCant@users.noreply.github.com>
2021-05-02 23:32:25 -0500

    walking's a thing
    but no flip

--- VogongUs/Assets/Scripts/PlayerControl.cs
+++ VogongUs/Assets/Scripts/PlayerControl.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Animator animator;
+    public Transform self;
+    public float runSpeed = .1f;
+    private float direction = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            direction = Input.GetAxisRaw("Horizontal");
+            animator.SetBool("isWalking", true);
+            self.position = new Vector2(self.position.x + (Input.GetAxisRaw("Horizontal")* runSpeed), self.position.y);
+        }
+        else if (Input.GetAxisRaw("Vertical") != 0)
+        {
+            animator.SetBool("isWalking", true);
+            self.position = new Vector2(self.position.x, self.position.y + (Input.GetAxisRaw("Vertical") * runSpeed));
+        }
+        else
+        {
+            animator.SetBool("isWalking", false);
+        }
+
+        /*VECTOR3 THESCALE = SELF.LOCALSCALE;
+        THESCALE.X *= SELF.LOCALSCALE.X * DIRECTION;
+        SELF.LOCALSCALE = THESCALE;*/
+
     }
 }
