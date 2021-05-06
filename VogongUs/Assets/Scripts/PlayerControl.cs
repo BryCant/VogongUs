55564e63b578bf2058e1101648c9391dc4a8a889
Bryant <33697753+BryCant@users.noreply.github.com>
2021-05-05 20:51:48 -0500

    Pause Menu Basics In
    -time stops
    -player movement disabled
    -sound button toggles sprite
    -pause toggle with continue btn works

--- VogongUs/Assets/Scripts/PlayerControl.cs
+++ VogongUs/Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,12 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
+    public GUI_Logic GUI_Object;
+
     public Animator animator;
     public Transform self;
     public float runSpeed = .1f;
+
     private Vector3 RIGHT_DIRECTION;
     private Vector3 LEFT_DIRECTION;
 
@@ -21,24 +24,28 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if(GUI_Object.isPaused == false)
         {
-            if (Input.GetAxisRaw("Horizontal") > 0)
+            if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
             {
-                self.localScale = RIGHT_DIRECTION;
-            }
-            else if (Input.GetAxisRaw("Horizontal") < 0)
-            {
-                self.localScale = LEFT_DIRECTION;
+                if (Input.GetAxisRaw("Horizontal") > 0)
+                {
+                    self.localScale = RIGHT_DIRECTION;
+                }
+                else if (Input.GetAxisRaw("Horizontal") < 0)
+                {
+                    self.localScale = LEFT_DIRECTION;
 
+                }
+                animator.SetBool("isWalking", true);
+                self.position = new Vector2(self.position.x + (Input.GetAxisRaw("Horizontal")* (runSpeed / 100)), self.position.y + (Input.GetAxisRaw("Vertical") * (runSpeed / 100)));
             }
-            animator.SetBool("isWalking", true);
-            self.position = new Vector2(self.position.x + (Input.GetAxisRaw("Horizontal")* (runSpeed / 100)), self.position.y + (Input.GetAxisRaw("Vertical") * (runSpeed / 100)));
+            else
+            {
+                animator.SetBool("isWalking", false);
+            }
         }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
+        
 
     }
 }
