3e133e37c9b04af2d5a30e46d57ed62301764d89
Bryant <33697753+BryCant@users.noreply.github.com>
2021-05-06 00:29:02 -0500

    Vogon Follow Logic Improved

--- VogongUs/Assets/Enemy_Behaviour.cs
+++ VogongUs/Assets/Enemy_Behaviour.cs
@@ -7,18 +7,42 @@ public class Enemy_Behaviour : MonoBehaviour
 {
 
     public AIPath aiPath;
+    public GameObject VogonGFX;
 
+    public Transform target;
+    public float minDistance = 1f;
+    private Vector3 startPosition;
+
+
+    private void Start()
+    {
+        startPosition = transform.position;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(aiPath.desiredVelocity.x >= 0.01f)
+        if(Vector2.Distance(transform.position, target.position) < minDistance)
         {
-            transform.localScale = new Vector3(1f, 1f, 1f);
+            aiPath.canMove = true;
+            if (aiPath.desiredVelocity.x >= 0.01f)
+            {
+                VogonGFX.transform.localScale = new Vector3(1f, 1f, 1f);
+            }
+            else if (aiPath.desiredVelocity.x <= 0.01f)
+            {
+                VogonGFX.transform.localScale = new Vector3(-1f, 1f, 1f);
+            }
         }
-        else if (aiPath.desiredVelocity.x <= 0.01f)
+        else
         {
-            transform.localScale = new Vector3(-1f, 1f, 1f);
+            aiPath.canMove = false;
+            if(Vector2.Distance(transform.position, startPosition) < (minDistance * 1.5))
+            {
+                transform.position = Vector2.MoveTowards(transform.position, startPosition, 1 * Time.deltaTime);
+            }
         }
+
+        
     }
 }
