ebc03d7c018551f21689b02e49f42c0af9fae00c
Bryant <33697753+BryCant@users.noreply.github.com>
2021-05-07 02:02:33 -0500

    Main Menu Be Existing

---
+++ VogongUs/Assets/MainMenu_Logic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu_Logic : MonoBehaviour
+{
+
+    public GameObject Charachters;
+    public GameObject Instructions;
+    public GameObject ChoosePlayer;
+    public GameObject HowToPlay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Charachters.SetActive(false);
+        Instructions.SetActive(false);
+    }
+
+    public void Choose()
+    {
+        Charachters.SetActive(true);
+    }
+
+    public void How()
+    {
+        Instructions.SetActive(true);
+    }
+
+    public void Arthur()
+    {
+        SceneManager.LoadScene("A_VogonShip");
+    }
+
+    public void Ford()
+    {
+        SceneManager.LoadScene("A_VogonShip");
+    }
+}
