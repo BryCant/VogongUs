7ade0088f32fa9f5f38436c3c61fe70b26680ec4
Bryant <33697753+BryCant@users.noreply.github.com>
2021-05-07 02:30:14 -0500

    Seconds are in

---
+++ VogongUs/Assets/Collectible_Logic.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectible_Logic : MonoBehaviour
+{
+
+    public SecondsHolder SecondsGUI;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SecondsGUI.seconds++;
+            Debug.Log(SecondsGUI.seconds);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ VogongUs/Assets/SecondsHolder.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondsHolder : MonoBehaviour
+{
+    public int seconds = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
